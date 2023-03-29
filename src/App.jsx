32a6ff6dd31cde3a9818c97b6f060841bb06b545
Alex Kitsu <9apsilva@gmail.com>
2023-03-29 18:53:55 -0300

    feat: add base structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Header } from './components/Header'
+import { Home } from './components/Home'
+import { Footer } from './components/Footer'
+
 export function App() {
   return (
-    <>
-    olá
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 
 }
