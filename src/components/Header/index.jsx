9f10691914c9485f53aee7f6d4a09d23f2d021fc
Alex Kitsu <9apsilva@gmail.com>
2023-04-01 13:09:37 -0300

    style: add style in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom'
 import * as S from './main.module.css'
+import React from 'react'
+import DogIcon from '../../assets/dogs.svg'
 
 export function Header() {
   return (
     <div className={S.header}>
-      <nav className='container'>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Logar</Link>
+      <nav className={`${S.nav} container`}>
+        <Link className={S.linkHome} to='/'>
+          <img src={DogIcon} alt='icone com a logo de Dogs'/>
+        </Link>
+        <Link className={S.linkLogin} to='/login'>Logar</Link>
       </nav>
     </div>
   )
