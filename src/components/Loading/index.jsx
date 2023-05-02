2901f0950f76194a60aacc2d6cf17cb455a4d33f
Alex Kitsu <9apsilva@gmail.com>
2023-05-01 21:00:25 -0300

    style: modify loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -2,8 +2,8 @@ import * as S from './main.module.css'
 
 export function Loading() {
   return(
-    <>
-    is loading
-    </>
+    <div className={S.wrapper}>
+      <div className={S.loading}></div>
+    </div>
   )
 }
